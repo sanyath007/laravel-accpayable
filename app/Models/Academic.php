54fb3ba6e06ba6ca532e3563a7deb40df6158fa2
sanyath007 <sanyath007@gmail.com>
2019-07-09 07:09:47 +0700

    Edited Academic model.

--- app/Models/Academic.php
+++ app/Models/Academic.php
@@ -8,7 +8,7 @@ class Academic extends Model
 {
   	protected $connection = 'person';
   	protected $table = 'academic';
-
+  	
   	public function user()
     {
         return $this->hasMany('App\Models\User', 'ac_id', 'ac_id');
